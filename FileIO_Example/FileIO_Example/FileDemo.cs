5df4133d54d41062106d39a8d86586f757bd1bbd
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-27 20:35:24 +0530

    Add:Check File is exists or not

---
+++ FileIO_Example/FileIO_Example/FileDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO_Example
+{
+    public class FileDemo
+    {
+        const string FilePath = @"D:\Bridgelab\FileIO_Example\FileIO_Example\data.txt";
+        public static void ReadData()
+        {
+            if(File.Exists(FilePath))
+            {
+                Console.WriteLine("File is exist");
+            }
+            else
+            {
+                Console.WriteLine("File is not exist");
+            }
+        }
+    }
+}
---
+++ FileIO_Example/FileIO_Example/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileIO_Example;
+
+bool status = true;
+
+while(status)
+{
+    Console.WriteLine("Select\n1)Check File is exits or not");
+    int op = Convert.ToInt32(Console.ReadLine());
+    switch(op)
+    {
+        case 1:
+            FileDemo.ReadData();
+            break;
+    }
+}
